d54da62a6798d5ba7bb634b4283299a21ab8d48d
MadakariNayakaHM <mahm20cs@cmrit.ac.in>
2022-12-04 19:04:06 +0530

    re running

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ const mongoose=require('mongoose');
 dotenv.config({path:'./config.env'});
 const port=process.env.PORT;
 const DB=process.env.DATABASE;
+const DB2=process.env.DATABASE2;
 console.log(`app is in ${process.env.NODE_ENV} mode`);
-try{
-mongoose.connect(DB).then(()=>{console.log("data base connected successfully")}).catch();
+if(process.env.NODE_ENV=="production")
+{
+    try{
+        mongoose.connect(DB).then(()=>{console.log("data base connected successfully")}).catch();
+        }
+        catch(e){
+        console.log("error happened at database connection");
+        console.log(e);
+        }
 }
-catch(e){
-console.log("error happened at database connection");
-console.log(e);
+else
+{
+    try{
+        mongoose.connect(DB2).then(()=>{console.log("data base connected successfully(development)")}).catch();
+        }
+        catch(e){
+        console.log("error happened at database connection");
+        console.log(e);
+        }
 }
 const app =require('./app');
 app.listen(port,()=>{console.log(`app is running at the port ${port}`)})
